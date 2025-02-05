1a6b005ec28fe53ff40ddd6a2185d8b8a2e250fe
nazar.khimin <nazar.khimin@unanet.com>
2025-02-05 20:41:07 +0200

    - updated method invocation chain

--- main.py
+++ main.py
@@ -8,22 +8,29 @@ school = School("СЗШ 32")
 student1 = Student("Nazar")
 student2 = Student("Bohdan")
 teacher1 = Teacher("Mykola")
-course = Course("Math", teacher1, 5, 4, 5)
+course1 = Course("Math", teacher1, 5, 4, 4)
+course2 = Course("Biology", teacher1, 5, 3, 4)
 
 # add teacher and student to course
-teacher1.add_course(course)
-course.add_student(student1)
-course.add_student(student2)
+teacher1.add_course(course1)
+course1.add_student(student1)
+course1.add_student(student2)
+teacher1.add_course(course2)
+course2.add_student(student1)
+course2.add_student(student2)
 
 # assign grade
-teacher1.assign_grade(course, student1, 4)
-teacher1.assign_grade(course, student2, 5)
+teacher1.assign_grade(course1, student1, 70)
+teacher1.assign_grade(course1, student1, 70)
+teacher1.assign_grade(course2, student2, 80)
+teacher1.assign_grade(course2, student2, 80)
 
 # add all to school
 school.add_student(student1)
 school.add_student(student2)
 school.add_teacher(teacher1)
-school.add_course(course)
+school.add_course(course1)
+school.add_course(course2)
 
 # calculate GPA for students
 gpa1 = student1.calculate_gpa()
@@ -32,4 +39,5 @@ print(f'GPA for student {student1.name} : {gpa1}')
 print(f'GPA for student {student2.name} : {gpa2}')
 
 # display information about students, teacher, and courses
-print(course)
+print(course1)
+print(course2)
