2afe0f0bfc4b79c00110b11ec9fafc09c876c1d7
nazar.khimin <nazar.khimin@unanet.com>
2025-02-07 21:56:10 +0200

    2. updated validation params names

--- src/utils/validations.py
+++ src/utils/validations.py
@@ -1,16 +1,16 @@
-def validate_grade_level(grade: int):
-    if not 1 <= grade <= 4:
+def validate_grade_level(grade_level: int):
+    if not 1 <= grade_level <= 4:
         raise ValueError("Grade level must be between 1 and 4")
-    return grade
+    return grade_level
 
 
-def validate_grade_value(grade: int):
-    if not 0 <= grade <= 100:
+def validate_grade_value(grade_value: int):
+    if not 0 <= grade_value <= 100:
         raise ValueError("Grade value must be between 0 and 100")
-    return grade
+    return grade_value
 
 
-def validate_grade_credits(grade: int):
-    if not 0 <= grade <= 5:
+def validate_grade_credits(grade_credits: int):
+    if not 0 <= grade_credits <= 5:
         raise ValueError("Grade value must be between 1 and 5")
-    return grade
+    return grade_credits
