2fb2e8eef23ffef9343fbc4fd1d8e2487674dbc3
nazar.khimin <nazar.khimin@unanet.com>
2025-02-18 12:24:36 +0200

    - remove unnecessary __repr__ since we have @generate_repr()

--- src/person.py
+++ src/person.py
@@ -9,6 +9,3 @@ class Person(ABC):
     def __init__(self, name):
         self.id = str(uuid.uuid4())
         self.name = name
-
-    def __repr__(self):
-        return f'id = {self.id!r}, name = {self.name!r}'
