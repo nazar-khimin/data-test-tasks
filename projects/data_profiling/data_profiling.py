27f2a389fb70e4252ba985a34f9d5b86a9f260ea
nazar.khimin <nazar.khimin@unanet.com>
2025-03-31 00:39:33 +0300

    added spark initialization to spark profiling script

--- projects/data_profiling/data_profiling.py
+++ projects/data_profiling/data_profiling.py
@@ -1,5 +1,11 @@
+from pyspark.sql import SparkSession
+
 from projects.data_profiling.profiling import generate_statistics
 from projects.data_profiling.anomaly_detection import detect_outliers, detect_iqr_outliers
+
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("Data Profiling").getOrCreate()
 
 # Sample DataFrame
 df = spark.createDataFrame([(1, 5), (2, 8), (3, 10), (4, 2)], ["id", "value"])
@@ -8,3 +14,5 @@ df = spark.createDataFrame([(1, 5), (2, 8), (3, 10), (4, 2)], ["id", "value"])
 generate_statistics(df)
 detect_outliers(df)
 detect_iqr_outliers(df)
+
+spark.stop()
