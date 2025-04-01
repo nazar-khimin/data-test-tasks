1f1198ea3ee7760466e5f09a20c13e6f4952035f
nazar.khimin <nazar.khimin@unanet.com>
2025-04-02 02:08:57 +0300

    added ranges validator and added check to notebook

--- projects/data_quality_validation/schema_validation.py
+++ projects/data_quality_validation/schema_validation.py
@@ -11,11 +11,11 @@ class SchemaValidation:
         extra_columns = set(schema.keys()) - set(self.df.columns)
         return {"missing_columns": list(missing_columns), "extra_columns": list(extra_columns)}
 
-    def check_data_types(self, schema: dict[str, str]):
+    def check_data_types(self, type_schema: dict[str, str]):
         mismatched_types = {}
         type_mapping = {"float": "float64", "int": "int64"}  # Mapping to pandas dtypes
 
-        for column, expected_type in schema.items():
+        for column, expected_type in type_schema.items():
             if column in self.df.columns:
                 actual_type = str(self.df[column].dtype)
                 mapped_expected_type = type_mapping.get(expected_type, expected_type)  # Map expected type
@@ -24,3 +24,15 @@ class SchemaValidation:
                     mismatched_types[column] = {"expected": mapped_expected_type, "actual": actual_type}
 
         return mismatched_types
+
+    def validate_ranges(self, ranges_schema: dict[str, tuple[float, float]]) -> str:
+        violations = []
+        for column, (min_value, max_value) in ranges_schema.items():
+            if column in self.df.columns:
+                invalid_rows = self.df[(self.df[column] < min_value) | (self.df[column] > max_value)]
+                if not invalid_rows.empty:
+                    violations.append(f"{len(invalid_rows)} invalid rows in column '{column}'.")
+        if not violations:
+            return "All values within expected ranges."
+        else:
+            return "\n".join(violations)
