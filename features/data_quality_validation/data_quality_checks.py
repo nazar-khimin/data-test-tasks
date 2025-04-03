4da36edea08e33abb84bfefa8ade534456d448aa
nazar.khimin <nazar.khimin@unanet.com>
2025-04-03 23:45:59 +0300

    Rewrote quality checks on pyspark

--- features/data_quality_validation/data_quality_checks.py
+++ features/data_quality_validation/data_quality_checks.py
@@ -1,22 +1,35 @@
+import logging
+
 import pandas as pd
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import count, when, col, isnan
+
 from features.utils.logger_config import logger
+
 
 class DataQualityChecks:
     def __init__(self, file_path):
         try:
-            self.df = pd.read_csv(file_path, delimiter=",")
+            self.spark = SparkSession.builder.getOrCreate()
+            self.df = self.spark.read.csv(file_path, header=True, inferSchema=True)
             logger.info("Dataset loaded successfully.")
-        except FileNotFoundError:
-            logger.info(f"File not found: {file_path}")
         except Exception as e:
             logger.info(f"Error while loading the file: {e}")
 
-    def check_null_values(self):
-        null_counts = self.df.isnull().sum()
-        return null_counts[null_counts > 0]
+    def check_null_values(self) -> DataFrame:
+        null_counts = [
+            count(when(col(column).isNull() | isnan(column), column)).alias(column)
+            for column in self.df.columns
+        ]
+        return self.df.select(null_counts)
 
-    def check_duplicates(self):
-        return self.df.duplicated().sum()
+    def check_duplicate(self) -> int:
+        duplicates = (
+            self.df.groupBy(self.df.columns)
+            .count()
+            .filter(col("count") > 1)
+        )
+        return duplicates.count()
 
     def get_dataframe(self):
         return self.df
